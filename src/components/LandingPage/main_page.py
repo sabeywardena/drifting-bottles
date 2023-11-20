7ca9fde8ad296381cb657b60b225f94896cb8517
sabeywardena <sarahabeywardena@gmail.com>
2023-11-20 00:03:03 -0500

    Code refractor

---
+++ src/components/LandingPage/main_page.py
@@ -0,0 +1,65 @@
+# main_page.py
+from kivy.graphics import Color, Rectangle
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.button import Button
+
+
+class IconLabel(ButtonBehavior, Label):
+    pass
+
+
+class MainPage(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        main_layout = BoxLayout(orientation='vertical')
+
+        # Header with icons
+        header = BoxLayout(size_hint_y=None, height=100, orientation='horizontal')
+        time_label = IconLabel(text='9:59', size_hint_x=None, width=100)
+
+        header.add_widget(time_label)
+        header.add_widget(Label(text='Drifting Bottle', halign='center'))
+        settings_button = Button(background_normal='setting.png', size_hint=(None, None), size=(90, 90))
+        settings_button.bind(on_press=self.go_to_settings)
+        header.add_widget(settings_button)
+        # Container for WiFi and Settings icons
+        icons_layout = BoxLayout(orientation='vertical', size_hint_x=None, width=50)
+        #wifi_icon = Image(source='wifi.png', size_hint=(None, None), size=(50, 50))
+        #settings_icon = Image(source='setting.png', size_hint=(None, None), size=(90, 90))
+        #icons_layout.add_widget(wifi_icon)
+        #icons_layout.add_widget(settings_icon)
+
+        header.add_widget(icons_layout)
+        main_layout.add_widget(header)
+
+        # Middle content area
+        middle_content = Label(text='you dont have messages right now')
+        main_layout.add_widget(middle_content)
+
+        # Footer with navigation icons
+        footer = BoxLayout(size_hint_y=None, height=50, orientation='horizontal',
+                           spacing=(Window.width - (3 * 120)) / 7)
+        bottle_button = Button(background_normal='bottle.png', size_hint=(None, None), size=(120, 120))
+        bottle_button.bind(on_press=self.go_to_create_bottle)
+        footer.add_widget(bottle_button)
+        footer.add_widget(Button(background_normal='search.png', size_hint=(None, None), size=(120, 120)))
+        profile_button = Button(background_normal='person.png', size_hint=(None, None), size=(120, 120))
+        profile_button.bind(on_press=self.go_to_profile)
+        footer.add_widget(profile_button)
+
+        main_layout.add_widget(footer)
+        self.add_widget(main_layout)
+
+    def go_to_create_bottle(self, instance):
+        self.manager.current = 'create_bottle'
+
+    def go_to_profile(self, instance):
+        self.manager.current = 'profile_page'
+
+    def go_to_settings(self, instance):
+        self.manager.current = 'settings'
--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-import kivy
+# main.py
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager
+from src.components.LandingPage.main_page import MainPage
+from src.components.ThrowBottle.create_bottle import CreateBottle
+from kivy.core.window import Window
+from src.components.Profile.profile_page import ProfilePage
+from src.components.Settings.settings import SettingsPage
 
-class MyApp(App):
+class DriftingBottleApp(App):
     def build(self):
-        return Label(text="Hello World")
+        sm = ScreenManager()
+        sm.add_widget(MainPage(name='main_page'))
+        sm.add_widget(SettingsPage(name='settings'))
+        sm.add_widget(CreateBottle(name='create_bottle'))
+        sm.add_widget(ProfilePage(name='profile_page'))
+        Window.size = (360, 640)
+        return sm
 
-
-if __name__ == "__main__" :
-    MyApp().run()
+if __name__ == '__main__':
+    DriftingBottleApp().run()
