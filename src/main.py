173e4b015200c3c31ba1b43b7cb650ae5937709c
sabeywardena <sarahabeywardena@gmail.com>
2023-11-20 01:52:23 -0500

    Added settings buttons

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from src.components.LandingPage.main_page import MainPage
+from src.components.Settings.background import Background
+from src.components.Settings.devices import Devices
+from src.components.Settings.notifications import Notifications
 from src.components.ThrowBottle.create_bottle import CreateBottle
 from kivy.core.window import Window
 from src.components.Profile.profile_page import ProfilePage
@@ -14,6 +17,9 @@ class DriftingBottleApp(App):
         sm.add_widget(SettingsPage(name='settings'))
         sm.add_widget(CreateBottle(name='create_bottle'))
         sm.add_widget(ProfilePage(name='profile_page'))
+        sm.add_widget(Notifications(name='notifications'))
+        sm.add_widget(Background(name='background'))
+        sm.add_widget(Devices(name='devices'))
         Window.size = (360, 640)
         return sm
 
