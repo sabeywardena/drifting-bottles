d4ece850242678c01ad5726f9382d77e49f62a88
sabeywardena <sarahabeywardena@gmail.com>
2023-11-20 14:10:45 -0500

    icons

--- src/components/LandingPage/main_page.py
+++ src/components/LandingPage/main_page.py
@@ -41,7 +41,7 @@ class MainPage(Screen):
         header.add_widget(spacer_right)
 
         # Settings button (right-aligned)
-        settings_button = Button(background_normal='assets/setting.png', size_hint=(None, None), size=(90, 90))
+        settings_button = Button(background_normal='assets/settings.png', size_hint=(None, None), size=(90, 90))
         settings_button.bind(on_press=self.go_to_settings)
         header.add_widget(settings_button)
 
@@ -59,17 +59,17 @@ class MainPage(Screen):
         bottle_button.bind(on_press=self.go_to_create_bottle)
         footer.add_widget(bottle_button)
         
-        search_button = Button(background_normal='assets/search.png', size_hint=(None, None), size=(120, 120))
+        search_button = Button(background_normal='assets/loupe.png', size_hint=(None, None), size=(120, 120))
         search_button.bind(on_press=self.go_to_search)
         footer.add_widget(search_button)
 
-        profile_button = Button(background_normal='assets/person.png', size_hint=(None, None), size=(120, 120))
+        profile_button = Button(background_normal='assets/user.png', size_hint=(None, None), size=(120, 120))
         profile_button.bind(on_press=self.go_to_profile)
         footer.add_widget(profile_button)
 
         # Add background color to the footer
         with footer.canvas.before:
-            Color(51/255, 83/255, 181/255, .7)  # RGBA color values (adjust as needed)
+            Color(251/255, 255/255, 241/255, .7)  # RGBA color values (adjust as needed)
             self.footer_background = Rectangle(pos=footer.pos, size=(Window.width, 120))
 
         # Bind the background to the layout's size and position changes
